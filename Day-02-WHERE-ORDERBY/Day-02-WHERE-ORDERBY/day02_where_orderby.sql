8619de49bff902d8757918a022d31c1d8a321305
Enjamul Hoque <enjamulhh48@gmail.com>
2026-07-08 11:35:55 +0530

    Create day02_where_orderby.sql

---
+++ Day-02-WHERE-ORDERBY/Day-02-WHERE-ORDERBY/day02_where_orderby.sql
@@ -0,0 +1,23 @@
+CREATE TABLE products
+(
+product_id SERIAL PRIMARY KEY,
+product_name VARCHAR(50),
+category VARCHAR(50),
+price INT
+);
+
+INSERT INTO products
+(product_name,category,price)
+
+VALUES
+
+('Mobile','watch',5000),
+('iphone','brand',5000),
+('tv','watch2',300),
+('tv2','watch3',1000),
+('tv3','watch',5000),
+('Mobile2','watch',500),
+('cycle','no1',500),
+('cenamatic','watchly',600),
+('Intersing Product','watchly',6000),
+('Mobile5','watch4',5000);
